237524a9dfa6b7bb00db46eebb22eb589d6e84e9
MaximSomochkin <m.somochkin@andersenlab.com>
2021-11-30 17:18:19 +0200

    Stage 1 - enter name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //STAGE1
+       final String bot_name = "Alice";
+        System.out.println("Enter your name:");
+        final Scanner scanner = new Scanner(System.in);
+        final int birth_day = 12;
+        final int birth_month = 10;
+        final int birth_year = 2021;
+        if(bot_name.equalsIgnoreCase(scanner.nextLine())) {
+            System.out.println("Hi, my name is " + bot_name);
+        }
+        System.out.println( "I was created " + birth_day + "." + birth_month + "." + birth_year );
     }
 }
