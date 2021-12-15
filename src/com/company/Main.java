ee77b7ddf269ea9d58d9eca9fca26ede276f32b2
MaximSomochkin <m.somochkin@andersenlab.com>
2021-12-15 19:31:10 +0200

    Java basics creating human and print name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,22 @@
 package com.company;
 
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //STAGE1
-       final String bot_name = "Alice";
-        System.out.println("Enter your name:");
-        final Scanner scanner = new Scanner(System.in);
-        final int birth_day = 12;
-        final int birth_month = 10;
-        final int birth_year = 2021;
-        if(bot_name.equalsIgnoreCase(scanner.nextLine())) {
-            System.out.println("Hi, my name is " + bot_name);
-        }
-        System.out.println( "I was created " + birth_day + "." + birth_month + "." + birth_year );
+        Human maxHuman = new Human();
+        
+        System.out.println(maxHuman.name);
+        maxHuman.printHello();
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.println("Please enter your name:");
+        String enteredName =  scanner.nextLine();
+        maxHuman.name = enteredName;
+        maxHuman.printHello();
+        
     }
 }
